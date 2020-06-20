0357ce6a20d4744984c5d31547a1586cafda70bd
Teo Oliver <teo.liz.oli@gmail.com>
2020-06-20 23:02:19 +0100

    removed yelp.js from git

--- src/api/yelp.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.yelp.com/v3/businesses",
-  headers: {
-    Authorization:
-      "Bearer gDclcdkDSMJSQNS6TxHQ7RGtUJlvuU1CAAAybx6ZBuXGA6Onz6PlPL8TUnNeeRQT1JieBlCoP_wOd8AcMaRaeZp8xfl1wKUHlnUI4uZYs57pxtTlqDp1OyxHmF_tXnYx",
-  },
-});
