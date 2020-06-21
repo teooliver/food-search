73fa4f206be413b95d7a8891237d2980c1453b37
Teo Oliver <teo.liz.oli@gmail.com>
2020-06-21 14:14:52 +0100

    added ResultDetail

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+
+const ResultsDetail = ({ result }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>{result.name}</Text>
+      <Image style={styles.image} source={{ uri: result.image_url }} />
+      <Text>
+        {result.rating} Stars,{result.review_count} Reviews
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 15,
+  },
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 5,
+  },
+  name: {
+    fontWeight: "bold",
+  },
+});
+
+export default ResultsDetail;
--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
+import ResultsDetail from "./ResultsDetail";
 
 const ResultsList = ({ title, results }) => {
   return (
@@ -10,7 +11,7 @@ const ResultsList = ({ title, results }) => {
         data={results}
         keyExtractor={(results) => results.id}
         renderItem={({ item }) => {
-          return <Text>{item.name}</Text>;
+          return <ResultsDetail result={item} />;
         }}
       />
     </View>
@@ -21,6 +22,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontWeight: "bold",
+    marginLeft: 15,
   },
 });
 
